1d1c37c7af99200d9506f705e41936046c207497
christen <christengeorge25@gmail.com>
2021-01-25 23:42:53 +0200

    finish create database

--- IC-database/createDB.sh
+++ IC-database/createDB.sh
@@ -1,5 +1,12 @@
 #!/bin/bash/
-echo "your choice is create DataBase : " 
-echo please enter DataBase name 
+echo "--------< your choice is to create DataBase>-------- " 
+echo "Please enter DataBase name : "
 read dbname
+bash search.sh "$dbname"
+if [ "$?" -eq "1" ]
+then
+	echo "DataBase Already Exsit !"
+else
+	mkdir ../DATABASES/$dbname
+fi
 echo your DB is $dbname
--- IC-database/search.sh
+++ IC-database/search.sh
@@ -2,22 +2,19 @@
 exist_flag="false"
 item=$1
 pos=$2
-echo $item
 function searchDB  {
-	echo seeeaaaarrrccchhhhh
+  #echo seeeaaaarrrccchhhhh
 
 	for element in `ls ../DATABASES/"$pos"`
                         do              
                            if test "$item" == "$element"
                                 then
                                 exist_flag="true"
-				echo $exist_flag
-
+				#echo $exist_flag
                                 return 1
                         else 
                                 exist_flag="false"
-				echo $exist_flag
-				#return 0
+				#echo $exist_flag
                            fi
                         done
 		return 0	
